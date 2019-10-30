2b867109a16db8f6a05a5251bc7c8772f2410d59
NghiemThuHien2110 <nghiemthuhien1221@gmail.com>
2019-10-30 11:42:31 +0700

    news_share

--- src/main/java/com/ifisolution/cmsmanagerment/common/CommonConstant.java
+++ src/main/java/com/ifisolution/cmsmanagerment/common/CommonConstant.java
@@ -6,4 +6,6 @@ public class CommonConstant {
 	public static final String TOPIC_NOT_FOUND = "Topic not found!";
 	public static final String NEWS_STATUS_NOT_FOUND = "News status not found!";
 	public static final String NEWS_HEADER_ALREADY_EXIST = "News header already exist!";
+
+	public static final String NEWS_SHARE_NOT_FOUND = "News share not found";
 }
