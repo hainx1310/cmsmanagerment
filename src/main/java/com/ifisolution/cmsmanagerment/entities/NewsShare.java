b137505583c936f6fcd6e7ac214f973d1dbfcd78
NghiemThuHien2110 <nghiemthuhien1221@gmail.com>
2019-10-30 10:34:08 +0700

    news_share

--- src/main/java/com/ifisolution/cmsmanagerment/entities/NewsShare.java
+++ src/main/java/com/ifisolution/cmsmanagerment/entities/NewsShare.java
@@ -7,10 +7,11 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.Table;
 
 @Entity
+
+@Table(name = "cms_news_share")
 public class NewsShare {
 
 	@Id
